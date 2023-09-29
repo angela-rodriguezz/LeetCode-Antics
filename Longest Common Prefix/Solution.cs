4889150aee37e06170fdba1601d2ba7939921863
Angela Rodriguez <angelamareyrodriguez@gmail.com>
2023-09-29 18:40:02 -0500

    Update Solution.cs

--- Longest Common Prefix/Solution.cs
+++ Longest Common Prefix/Solution.cs
@@ -1,19 +1,13 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
         StringBuilder returner = new StringBuilder();
-        if (strs.Contains("")) {
-            return "";
-        } if (strs.Length == 1) {
-            return strs[0];
-        }
         for (int i = 0; i < strs[0].Length; i++) {
             for (int j = 1; j < strs.Length; j++) {
                 if (strs[j].Length <= i || strs[j][i] != strs[0][i]) {
                     return returner.ToString();
-                } else if (j == strs.Length - 1 && strs[j][i] == strs[0][i]) {
-                    returner.Append(strs[j][i]);
-                }
+                } 
             }
+            returner.Append(strs[0][i]);
         }
         return returner.ToString();
     }
