66e542c10707b78096a68f649701e267dc11fc36
Angela Rodriguez <angelamareyrodriguez@gmail.com>
2024-07-13 03:36:27 -0500

    Create Solution.cs

---
+++ Invert Binary Tree/Solution.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+            TreeNode righter = invertTree(root.right);
+            TreeNode lefter = invertTree(root.left);
+            root.left = righter;
+            root.right = lefter;
+            return root;
+    }
+}
